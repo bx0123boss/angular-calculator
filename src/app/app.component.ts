4e878d1ac260214cb6f185a2c570bbec8294c746
Brandon Huerta Castañeda <bran.7594@gmail.com>
2025-01-14 17:00:39 -0600

    Adding result table of investment and put in new component for visualization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  
+ resultsData?: {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+  totalInterest: number;
+  totalAmountInvested: number;
+}[];
 onCalculateInvestmentResults(data: InvestmentInput) {
   const annualData = [];
   const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
@@ -30,8 +38,7 @@ onCalculateInvestmentResults(data: InvestmentInput) {
       totalAmountInvested: initialInvestment + annualInvestment * year,
     });
   }
-
-  console.log(annualData);
+  this.resultsData=annualData;
 }
 
 }
